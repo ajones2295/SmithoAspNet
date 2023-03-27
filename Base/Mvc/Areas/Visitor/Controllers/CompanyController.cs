300db687db33b7634d93ff2c35b7587323849110
DaCrunkness <jerell.smith.09@gmail.com>
2023-03-27 18:01:15 -0500

    Add company

---
+++ Base/Mvc/Areas/Visitor/Controllers/CompanyController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mvc.Areas.Visitor.Controllers
+{
+    [Area("Visitor")]
+
+    public class CompanyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Careers()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Blogs()
+        {
+            return View();
+        }
+
+        public IActionResult News()
+        {
+            return View();
+        }
+
+        public IActionResult Clients()
+        {
+            return View();
+        }
+
+        public IActionResult Affiliates()
+        {
+            return View();
+        }
+    }
+}
---
+++ Base/Mvc/Areas/Visitor/Controllers/TechnologyController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mvc.Areas.Visitor.Controllers
+{
+    [Area("Visitor")]
+
+    public class TechnologyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
