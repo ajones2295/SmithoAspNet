f86f25978b43ad47f968a12771eeb1ff2f6ee60f
DaCrunkness <jerell.smith.09@gmail.com>
2023-03-27 18:20:02 -0500

    Added technology

--- Base/Mvc/Areas/Visitor/Controllers/TechnologyController.cs
+++ Base/Mvc/Areas/Visitor/Controllers/TechnologyController.cs
@@ -10,5 +10,111 @@ namespace Mvc.Areas.Visitor.Controllers
         {
             return View();
         }
+
+        public IActionResult Net()
+        {
+            return View();
+        }
+
+        public IActionResult Java()
+        {
+            return View();
+        }
+        public IActionResult Python()
+        {
+            return View();
+        }
+        public IActionResult Cpp()
+        {
+            return View();
+        }
+        public IActionResult Javascript()
+        {
+            return View();
+        }
+        public IActionResult Swift()
+        {
+            return View();
+        }
+
+        public IActionResult MySQL()
+        {
+            return View();
+        }
+
+        public IActionResult MSSQL()
+        {
+            return View();
+        }
+
+        public IActionResult SQLite()
+        {
+            return View();
+        }
+        public IActionResult NoSQL()
+        {
+            return View();
+        }
+
+        // Next Gen
+        public IActionResult Cloud()
+        {
+            return View();
+        }
+        public IActionResult IoT()
+        {
+            return View();
+        }
+        public IActionResult BigData()
+        {
+            return View();
+        }
+        public IActionResult DataScience()
+        {
+            return View();
+        }
+        public IActionResult AI()
+        {
+            return View();
+        }
+        public IActionResult Blockchain()
+        {
+            return View();
+        }
+        public IActionResult VR()
+        {
+            return View();
+        }
+        public IActionResult AR()
+        {
+            return View();
+        }
+
+        public IActionResult OpenData()
+        {
+            return View();
+        }
+
+        // Platforms
+
+        public IActionResult Microsoft()
+        {
+            return View();
+        }
+
+        public IActionResult Linux()
+        {
+            return View();
+        }
+
+        public IActionResult Apple()
+        {
+            return View();
+        }
+
+        public IActionResult Android()
+        {
+            return View();
+        }
     }
 }
