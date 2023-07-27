2b8d10aa925f5292603da1db70b71e2b102726c4
DaCrunkness <jerell.smith.09@gmail.com>
2023-07-27 15:52:41 -0500

    finalized services

---
+++ Base/Models/ViewModels/ConsultContactVM.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models.ViewModels
+{
+    public class ConsultContactVM
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        public string EmailAddress { get; set; } = string.Empty;
+
+        [Required]
+        [Phone(ErrorMessage = "Please enter a valid phone number.")]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string CompanyName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(400)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
